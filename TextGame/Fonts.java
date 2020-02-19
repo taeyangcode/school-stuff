ef193a9309e517f15273d60bad326461e0d6558f
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-19 02:02:20 -0800

    Create Fonts.java

---
+++ TextGame/Fonts.java
@@ -0,0 +1,27 @@
+package com.jetbrains;
+
+import java.awt.*;
+
+public class Fonts {
+
+    Font timesMainFont() {
+        Font timesMainFont = new Font("Times", Font.PLAIN, 28);
+        return timesMainFont;
+    }
+
+    Font timesSubFont() {
+        Font timesSubFont = new Font("Times", Font.PLAIN, 22);
+        return timesSubFont;
+    }
+
+    Font loseFont() {
+        Font loseFont = new Font("Times", Font.PLAIN, 48);
+        return loseFont;
+    }
+
+    Font winFont() {
+        Font winFont = new Font("Times", Font.PLAIN, 48);
+        return winFont;
+    }
+
+}
